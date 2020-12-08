3f62383f12027205422c866f3a2b12cf389fd3c3
Anthony Giorgio <agiorgio@us.ibm.com>
2020-12-08 15:28:00 -0500

    Update license text.

--- zOS-Tools-and-Toys/ext/ext.sh
+++ zOS-Tools-and-Toys/ext/ext.sh
@@ -1,12 +1,43 @@
 #!/bin/sh
+###################################################################
+# Copyright 2000-2020 IBM Corp.
 #
-# ext extracts files from an archive using ASCII to EBCDIC character translation and then 
-# goes back and identifies binary files (by file suffix) and re-extracts them without 
-# character translation.
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
 #
-# Copyright IBM Corp. 2000
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing,
+#  software distributed under the License is distributed on an
+#  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+#  either express or implied. See the License for the specific
+#  language governing permissions and limitations under the
+#  License.
+#
+# -----------------------------------------------------------------
+#
+# Disclaimer of Warranties:
+#
+#   The following enclosed code is sample code created by IBM
+#   Corporation.  This sample code is not part of any standard
+#   IBM product and is provided to you solely for the purpose
+#   of assisting you in the development of your applications.
+#   The code is provided "AS IS", without warranty of any kind.
+#   IBM shall not be liable for any damages arising out of your
+#   use of the sample code, even if they have been advised of
+#   the possibility of such damages.
+#
+# -----------------------------------------------------------------
+#
+# ext extracts files from an archive using ASCII to EBCDIC
+# character translation and then goes back and identifies binary
+# files (by file suffix) and re-extracts them without character
+# translation.
 #
 # Author: Mike MacIssac <mikemac@us.ibm.com>
+#
+###################################################################
 
 
 function usage
