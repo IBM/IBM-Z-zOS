7e91e80acb3264a0543cc40b488ef194f5ca53be
Anthony Giorgio <agiorgio@us.ibm.com>
2020-12-08 15:53:31 -0500

    Update license text

--- zOS-Tools-and-Toys/ifind/README.md
+++ zOS-Tools-and-Toys/ifind/README.md
@@ -1,11 +1,5 @@
 # ifind
 
-Copyright 1994, IBM Corporation
-
-All rights reserved
-
-## Description
-
 ifind is a tool that can be used to find files in a filesystem that share the same data.  A file can be known by several different links (sometimes called a "hard link" to distinguish them from "symbolic links) and they all point to the same "inode" - a number which uniquely identifies the file in the filesystem.  You can search for files by specifying the name of one of the links to the file or by specifying the inode as a decimal number.  If you specify the inode, ifind assumes you mean you want to search the filesystem containing the current working directory.  Since ifind assumes a decimal number is an inode, if you want to search for a file by its name which is composed of only digits (such as "1"), you must specify it in a way that it is unambiguous ("./1" for instance).
 
 ## Example
@@ -18,4 +12,3 @@ ifind is a tool that can be used to find files in a filesystem that share the sa
     /bin/read
     /bin/wait
     /bin/IBM/FSUMSSHB
-
--- zOS-Tools-and-Toys/ifind/ifind.c
+++ zOS-Tools-and-Toys/ifind/ifind.c
@@ -1,15 +1,44 @@
-/*
- *  Author: John Pfuntner
- *  Program: ifind
- *
- *  Copyright 1994, IBM Corporation
- *  All rights reserved
- *
- *  Purpose: Locates files in a filesystem with the same
- *  inode as a specified filename or inode.
- *
- *  Syntax: ifind {filename|inode} [...]
- */
+/**********************************************************************
+** Copyright 1994-2020 IBM Corp.
+**
+**  Licensed under the Apache License, Version 2.0 (the "License");
+**  you may not use this file except in compliance with the License.
+**  You may obtain a copy of the License at
+**
+**     http://www.apache.org/licenses/LICENSE-2.0
+**
+**  Unless required by applicable law or agreed to in writing,
+**  software distributed under the License is distributed on an
+**  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+**  either express or implied. See the License for the specific
+**  language governing permissions and limitations under the
+**  License.
+**
+** -----------------------------------------------------------------
+**
+** Disclaimer of Warranties:
+**
+**   The following enclosed code is sample code created by IBM
+**   Corporation.  This sample code is not part of any standard
+**   IBM product and is provided to you solely for the purpose
+**   of assisting you in the development of your applications.
+**   The code is provided "AS IS", without warranty of any kind.
+**   IBM shall not be liable for any damages arising out of your
+**   use of the sample code, even if they have been advised of
+**   the possibility of such damages.
+**
+** -----------------------------------------------------------------
+**
+**  Author: John Pfuntner <pfuntner@pobox.com>
+**
+**  Program: ifind
+**
+**  Purpose: Locates files in a filesystem with the same
+**  inode as a specified filename or inode.
+**
+**  Syntax: ifind {filename|inode} [...]
+**
+**********************************************************************/
 
 #define _OPEN_SYS
 #include <stdio.h>
