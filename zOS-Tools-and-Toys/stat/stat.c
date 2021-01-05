4f75a187d8f3f1ce8238177a68006a006ab6e761
Anthony Giorgio <agiorgio@us.ibm.com>
2021-01-05 14:50:02 -0500

    Add license text

--- zOS-Tools-and-Toys/stat/stat.c
+++ zOS-Tools-and-Toys/stat/stat.c
@@ -1,35 +1,46 @@
-#/* Copyright 1994, IBM Corporation
-# * All rights reserved
-# *
-# * Distribute freely, except: don't remove my name from the source or
-# * documentation (don't take credit for my work), mark your changes
-# * (don't get me blamed for your possible bugs), don't alter or
-# * remove this notice.  No fee may be charged if you distribute the
-# * package (except for such things as the price of a disk or tape,
-# * postage, etc.).  No warranty of any kind, express or implied, is
-# * included with this software; use at your own risk, responsibility
-# * for damages (if any) to anyone resulting from the use of this
-# * software rests entirely with the user.
-# *
-# * Send me bug reports, bug fixes, enhancements, requests, flames,
-# * etc.  I can be reached as follows:
-# *
-# *          John Pfuntner      John_Pfuntner@tivoli.com
-# */
-
-/*                                                       */
-/*  Author: John Pfuntner                                */
-/*  Program: stat                                        */
-/*  Version: 1.1                                         */
-/*                                                       */
-/*  Purpose: Displays stat() information on specified    */
-/*  filenames.                                           */
-/*                                                       */
-/*  Syntax: stat file [...]                              */
-/*                                                       */
-/*  change history:                                      */
-/*    version 1.0: July 1994: original release           */
-/*    version 1.1: July 1997: fixed <stat.h> include     */
+/**********************************************************************
+** Copyright 1994-2020 IBM Corp.
+**
+**  Licensed under the Apache License, Version 2.0 (the "License");
+**  you may not use this file except in compliance with the License.
+**  You may obtain a copy of the License at
+**
+**     http://www.apache.org/licenses/LICENSE-2.0
+**
+**  Unless required by applicable law or agreed to in writing,
+**  software distributed under the License is distributed on an
+**  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+**  either express or implied. See the License for the specific
+**  language governing permissions and limitations under the
+**  License.
+**
+** -----------------------------------------------------------------
+**
+** Disclaimer of Warranties:
+**
+**   The following enclosed code is sample code created by IBM
+**   Corporation.  This sample code is not part of any standard
+**   IBM product and is provided to you solely for the purpose
+**   of assisting you in the development of your applications.
+**   The code is provided "AS IS", without warranty of any kind.
+**   IBM shall not be liable for any damages arising out of your
+**   use of the sample code, even if they have been advised of
+**   the possibility of such damages.
+**
+** -----------------------------------------------------------------
+**  Author: John Pfuntner <pfuntner@pobox.com>
+**  Program: stat
+**  Version: 1.1
+**
+**  Purpose: Displays stat() information on specified
+**  filenames.
+**
+**  Syntax: stat file [...]
+**
+**  change history:
+**    version 1.0: July 1994: original release
+**    version 1.1: July 1997: fixed <stat.h> include
+***********************************************************************/
 
 #define _OPEN_SYS
 #include <sys/mntent.h>
