df51e04a023bf37870d3b17d79d606f686347036
Anthony Giorgio <agiorgio@us.ibm.com>
2022-02-24 16:05:21 -0500

    Update license text

--- zOS-Tools-and-Toys/auto-conversion/celquopt.s
+++ zOS-Tools-and-Toys/auto-conversion/celquopt.s
@@ -1,16 +1,33 @@
-*/****************************************************************/
-*/* LICENSED MATERIALS - PROPERTY OF IBM                         */
-*/*                                                              */
-*/* 5650-ZOS                                                     */
-*/*                                                              */
-*/*     COPYRIGHT IBM CORP. 2004, 2013                           */
-*/*                                                              */
-*/* US GOVERNMENT USERS RESTRICTED RIGHTS - USE,                 */
-*/* DUPLICATION OR DISCLOSURE RESTRICTED BY GSA ADP              */
-*/* SCHEDULE CONTRACT WITH IBM CORP.                             */
-*/*                                                              */
-*/* STATUS = HLE7790                                             */
-*/****************************************************************/
+****************************************************************
+* Copyright 2022 IBM Corp.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*   http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing,
+* software distributed under the License is distributed on an
+* "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+* either express or implied. See the License for the specific
+* language governing permissions and limitations under the
+* License.
+*
+* --------------------------------------------------------------
+*
+* Disclaimer of Warranties:
+*
+*   The following enclosed code is sample code created by IBM
+*   Corporation.  This sample code is not part of any standard
+*   IBM product and is provided to you solely for the purpose
+*   of assisting you in the development of your applications.
+*   The code is provided "AS IS", without warranty of any kind.
+*   IBM shall not be liable for any damages arising out of your
+*   use of the sample code, even if they have been advised of
+*   the possibility of such damages.
+*
+*****************************************************************/
 CELQUOPT CSECT
 CELQUOPT AMODE 64
 CELQUOPT RMODE ANY
